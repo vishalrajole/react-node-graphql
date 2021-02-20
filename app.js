aab553f63a65b605df8f7a1e6041282d1299e885
vishalrajole <vishalrajole1991@gmail.com>
2021-02-20 19:00:13 +0100

    express setup

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get("/", (req, res, next) => {
+  res.send("hellp");
+});
+app.listen(3000, () => {
+  console.log("app listening...");
+});
