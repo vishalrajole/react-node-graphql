344ae0a7ab13416c3f34adaf35a5488a06892071
vishalrajole <vishalrajole1991@gmail.com>
2021-02-20 19:32:02 +0100

    added events query

--- app.js
+++ app.js
@@ -11,12 +11,27 @@ app.use(
   "/graphql",
   graphqlHTTP({
     schema: buildSchema(`
+    type Event {
+        _id: ID!
+        title: String!
+        description: String!
+        price: Float!
+        date: String!
+    }
+
+    input EventInput {
+        title: String!
+        description: String!
+        price: Float!
+        date: String!
+    }
+
     type RootQuery {
-        events: [String!]!
+        events: [Event!]!
     }
 
     type RootMutation {
-        createEvent(name: String): String
+        createEvent(eventInput: EventInput): Event
     }
 
     schema {
@@ -32,8 +47,15 @@ app.use(
         return ["dummy", "test"];
       },
       createEvent: (args) => {
-        const name = args.name;
-        return name;
+        const event = {
+          _id: Math.random().toString(),
+          title: args.title,
+          description: args.description,
+          price: +args.price,
+          date: new Date().toISOString(),
+        };
+
+        return event;
       },
     },
   })
