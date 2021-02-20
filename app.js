857c4fce62040fbd515bb385222e55a39966b354
vishalrajole <vishalrajole1991@gmail.com>
2021-02-20 19:22:43 +0100

    graphql setup and mocked api

--- app.js
+++ app.js
@@ -1,9 +1,43 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const { graphqlHTTP } = require("express-graphql");
+const { buildSchema } = require("graphql");
 
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: buildSchema(`
+    type RootQuery {
+        events: [String!]!
+    }
+
+    type RootMutation {
+        createEvent(name: String): String
+    }
+
+    schema {
+        query: RootQuery , 
+        mutation: RootMutation
+    }
+    `),
+    graphiql: true,
+
+    //resolvers
+    rootValue: {
+      events: () => {
+        return ["dummy", "test"];
+      },
+      createEvent: (args) => {
+        const name = args.name;
+        return name;
+      },
+    },
+  })
+);
 
 app.get("/", (req, res, next) => {
   res.send("hellp");
