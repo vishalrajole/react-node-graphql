45efc35f9db44c5dbc713eb11f9535422bf72c7a
vishalrajole <vishalrajole1991@gmail.com>
2021-02-21 10:52:06 +0100

    restructured schema and resolvers

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const bcrypt = require("bcryptjs");
+
 const { graphqlHTTP } = require("express-graphql");
-const { buildSchema } = require("graphql");
 const mongoose = require("mongoose");
-
-const Event = require("./models/event");
-const User = require("./models/user");
-
+const graphiqlSchema = require("./graphql/schema/index");
+const graphqlResolver = require("./graphql/resolvers/index");
 const app = express();
 
 app.use(bodyParser.json());
@@ -15,163 +12,9 @@ app.use(bodyParser.json());
 app.use(
   "/graphql",
   graphqlHTTP({
-    schema: buildSchema(`
-    type Event {
-        _id: ID!
-        title: String!
-        description: String!
-        price: Float!
-        date: String!
-        creator: User!
-    }
-
-    type User {
-        _id: ID!,
-        email: String!
-        password: String
-        createdEvents: [Event!]
-    }
-
-    input EventInput {
-        title: String!
-        description: String!
-        price: Float!
-        date: String!
-    }
-
-    input UserInput {
-        email: String!
-        password: String!
-    }
-
-    type RootQuery {
-        events: [Event!]!
-    }
-
-    type RootMutation {
-        createEvent(eventInput: EventInput): Event
-        createUser(userInput: UserInput): User
-    }
-
-    schema {
-        query: RootQuery , 
-        mutation: RootMutation
-    }
-    `),
+    schema: graphiqlSchema,
     graphiql: true,
-
-    //resolvers
-    rootValue: {
-      events: async () => {
-        try {
-          return await Event.find();
-        } catch (error) {
-          throw new Error("failed to fetch events", error);
-        }
-
-        // return Event.find()
-        //   .then((events) => {
-        //     return events.map((event) => {
-        //       return { ...event._doc };
-        //     });
-        //   })
-        //   .catch((error) => {
-        //     console.log("failed to fetch events", events);
-        //   });
-      },
-      createEvent: async (args) => {
-        const mockedUser = "60322358f800af693edd11de";
-        const { title, description, price, date } = args.eventInput;
-        try {
-          const event = new Event({
-            title,
-            description,
-            price: +price,
-            date,
-            creator: mockedUser,
-          });
-          const savedEvent = await event.save();
-          const user = await User.findById(mockedUser);
-          if (!user) {
-            throw new Error("event creating user not found");
-          }
-          await user.createdEvents.push(savedEvent);
-          await user.save();
-          return savedEvent;
-        } catch (error) {
-          throw new Error("create event failed", error);
-        }
-        // const event = new Event({
-        //   title: args.eventInput.title,
-        //   description: args.eventInput.description,
-        //   price: +args.eventInput.price,
-        //   date: new Date(args.eventInput.date),
-        //   creator: "60321a9112380a637229707e",
-        // });
-        // let createdEvent;
-        // return event
-        //   .save()
-        //   .then((result) => {
-        //     createdEvent = { ...result._doc };
-        //     return User.findById("60321a9112380a637229707e")
-        //       .then((user) => {
-        //         if (!user) {
-        //           throw new Error("Creating user not found");
-        //         }
-        //         user.createdEvents.push(event);
-        //         return user.save();
-        //       })
-        //       .then((result) => {
-        //         return createdEvent;
-        //       });
-        //   })
-        //   .catch((error) => {
-        //     console.log("event save error", error);
-        //     throw error;
-        //   });
-      },
-      createUser: async (args) => {
-        const { email, password } = args.userInput;
-        try {
-          const existingUser = await User.findOne({ email });
-          if (existingUser) {
-            throw new Error("User already exists");
-          }
-          const hashedPassword = await bcrypt.hash(password, 12);
-          const user = new User({
-            email,
-            password: hashedPassword,
-          });
-          const result = await user.save();
-          return result;
-        } catch (error) {
-          throw new Error("create user failed", error);
-        }
-        // return User.findOne({ email: args.userInput.email })
-        //   .then((user) => {
-        //     if (user) {
-        //       throw new Error("User exists already");
-        //     }
-        //     return bcrypt.hash(args.userInput.password, 12);
-        //   })
-        //   .then((hashedPassword) => {
-        //     const user = new User({
-        //       email: args.userInput.email,
-        //       password: hashedPassword,
-        //     });
-
-        //     return user
-        //       .save()
-        //       .then((result) => {
-        //         return { ...result._doc };
-        //       })
-        //       .catch((error) => {
-        //         console.log("user save error", error);
-        //         throw error;
-        //       });
-        //   });
-      },
-    },
+    rootValue: graphqlResolver,
   })
 );
 
---
+++ graphql/resolvers/index.js
@@ -0,0 +1,115 @@
+const Event = require("../../models/event");
+const User = require("../../models/user");
+const bcrypt = require("bcryptjs");
+
+module.exports = {
+  events: async () => {
+    try {
+      return await Event.find().populate("creator");
+    } catch (error) {
+      throw new Error("failed to fetch events", error);
+    }
+
+    // return Event.find()
+    //   .then((events) => {
+    //     return events.map((event) => {
+    //       return { ...event._doc };
+    //     });
+    //   })
+    //   .catch((error) => {
+    //     console.log("failed to fetch events", events);
+    //   });
+  },
+  createEvent: async (args) => {
+    const mockedUser = "60322358f800af693edd11de";
+    const { title, description, price, date } = args.eventInput;
+    try {
+      const event = new Event({
+        title,
+        description,
+        price: +price,
+        date,
+        creator: mockedUser,
+      });
+      const savedEvent = await event.save();
+      const user = await User.findById(mockedUser);
+      if (!user) {
+        throw new Error("event creating user not found");
+      }
+      await user.createdEvents.push(savedEvent);
+      await user.save();
+      return savedEvent;
+    } catch (error) {
+      throw new Error("create event failed", error);
+    }
+    // const event = new Event({
+    //   title: args.eventInput.title,
+    //   description: args.eventInput.description,
+    //   price: +args.eventInput.price,
+    //   date: new Date(args.eventInput.date),
+    //   creator: "60321a9112380a637229707e",
+    // });
+    // let createdEvent;
+    // return event
+    //   .save()
+    //   .then((result) => {
+    //     createdEvent = { ...result._doc };
+    //     return User.findById("60321a9112380a637229707e")
+    //       .then((user) => {
+    //         if (!user) {
+    //           throw new Error("Creating user not found");
+    //         }
+    //         user.createdEvents.push(event);
+    //         return user.save();
+    //       })
+    //       .then((result) => {
+    //         return createdEvent;
+    //       });
+    //   })
+    //   .catch((error) => {
+    //     console.log("event save error", error);
+    //     throw error;
+    //   });
+  },
+  createUser: async (args) => {
+    const { email, password } = args.userInput;
+    try {
+      const existingUser = await User.findOne({ email });
+      if (existingUser) {
+        throw new Error("User already exists");
+      }
+      const hashedPassword = await bcrypt.hash(password, 12);
+      const user = new User({
+        email,
+        password: hashedPassword,
+      });
+      const result = await user.save();
+      return result;
+    } catch (error) {
+      throw new Error("create user failed", error);
+    }
+    // return User.findOne({ email: args.userInput.email })
+    //   .then((user) => {
+    //     if (user) {
+    //       throw new Error("User exists already");
+    //     }
+    //     return bcrypt.hash(args.userInput.password, 12);
+    //   })
+    //   .then((hashedPassword) => {
+    //     const user = new User({
+    //       email: args.userInput.email,
+    //       password: hashedPassword,
+    //     });
+
+    //     return user
+    //       .save()
+    //       .then((result) => {
+    //         return { ...result._doc };
+    //       })
+    //       .catch((error) => {
+    //         console.log("user save error", error);
+    //         throw error;
+    //       });
+    //   });
+  },
+};
